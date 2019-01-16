b6a00749dabeaf9cf8a65ccce5b82ccb5f286ff6
your name here <nickbielak95@gmail.com>
2019-01-16 14:53:50 -0800

    Finish Hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    sum = 0
+    sum = self.map.with_index {|el, idx| el * idx}.reduce(:+)
+    sum.hash
   end
 end
 
 class String
   def hash
+    self.split("").map{|el| el.ord}.hash
   end
 end
 
@@ -16,6 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.map{|sub| sub.map{|el| el.to_s}.hash}.hash
   end
 end
